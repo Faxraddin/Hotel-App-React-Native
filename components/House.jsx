b512848f47c07f7c9c2cb0f60b04055d21344f64
Faxraddin <reydivonay@gmail.com>
2023-11-03 23:05:48 +0400

    new page

---
+++ components/House.jsx
@@ -0,0 +1,29 @@
+import { View,Text,Image,TouchableOpacity } from "react-native";
+
+export default function HouseBox(props){
+    return(
+        <View style={{width:315,justifyContent:'center',alignItems:'center'}}>
+            <View style={{width:'100%',justifyContent:'center',alignItems:'center',gap:20}}>
+
+                <Image style={{width:'100%',height:400,borderRadius:20}} resizeMode="cover" source={props.img}/> 
+                <View style={{padding:10,paddingTop:30,paddingBottom:30,borderWidth:1,borderRadius:20,gap:3,justifyContent:'space-between',backgroundColor:'white',width:'100%',height:380}}>
+                    <Text style={{fontWeight:600,fontSize:30}}>House {props.houseNumber}</Text>
+                    <Text style={{fontWeight:300,fontSize:18}}>{props.houseText}</Text>
+                    <Image style={{width:300,position:'relative',top:-20}} resizeMode="contain" source={require('../public/logoss.png')}/>
+                    <View style={{width:'83%',borderLeftWidth: 1,gap:10,paddingLeft:20,position:'relative',left:30,top:-35}}>
+                        <Text style={{fontWeight:300,fontSize:18}}>Price</Text>
+                        <Text style={{fontWeight:300,fontSize:18}}>{props.price}</Text>
+                    </View>
+                    <View style={{borderRadius:20,marginTop:20,position:'relative',top:-30}}>
+                        <TouchableOpacity style={{alignItems:'center',justifyContent:'center',borderRadius:20}}>
+                            <View style={{width:'96%',borderRadius:15}}>
+                                <Text style={{backgroundColor:'black',color:'white',width:'100%',textAlign:'center',borderRadius:20,padding:10,fontWeight:300,fontSize:18}}>More Info</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </View>
+        </View>
+    )
+}
--- pages/Houses.jsx
+++ pages/Houses.jsx
@@ -1,11 +1,34 @@
-import { View,Text } from "react-native";
+import { View,Text,ImageBackground,ScrollView } from "react-native";
+
+import HouseBox from "../components/House";
 
 export default function Houses(){
+
+    const data = [
+        {id:1,houseNumber:1,houseText:'Charming village-style rental house with a bedroom, spacious living room...',beds:'king',price:100 ,img:require('../public/ev1.jpeg'),img1:'/ev11.jpg',img2:'/ev12.jpg',img3:'/ev13.jpg',img4:'/ev14.jpg',img5:'/ev15.jpg'},
+        {id:2,houseNumber:2,houseText:"Discover the enchanting allure of our two-story village-style rental house, where...",beds:'king',price:160 ,img:require('../public/house2.jpeg'),img1:'/ev11.jpg',img2:'/ev12.jpg',img3:'/ev13.jpg',img4:'/ev14.jpg',img5:'/ev15.jpg'},
+        {id:3,houseNumber:3,houseText:'Welcome to our delightful three-room village-style rental house, exuding rustic...',beds:'king',price:140 ,img:require('../public/ev3.jpeg'),img1:'/ev11.jpg',img2:'/ev12.jpg',img3:'/ev13.jpg',img4:'/ev14.jpg',img5:'/ev15.jpg'},
+        {id:4,houseNumber:4,houseText:'Embrace the serenity of our enchanting village-style rental property, featuring a...',beds:'king',price:80 ,img:require('../public/house4.jpg'),img1:'/ev11.jpg',img2:'/ev12.jpg',img3:'/ev13.jpg',img4:'/ev14.jpg',img5:'/ev15.jpg'},
+        {id:5,houseNumber:5,houseText:'Discover the pinnacle of comfort in our final village-style rental house, where...',beds:'king',price:110 ,img:require('../public/ev5.jpeg'),img1:'/ev11.jpg',img2:'/ev12.jpg',img3:'/ev13.jpg',img4:'/ev14.jpg',img5:'/ev15.jpg'},
+        {id:6,houseNumber:6,houseText:'Experience the epitome of comfort and charm in our final village-style rental...',beds:'king',price:110 ,img:require('../public/house6.jpeg'),img1:'/ev11.jpg',img2:'/ev12.jpg',img3:'/ev13.jpg',img4:'/ev14.jpg',img5:'/ev15.jpg'},
+    ];
+
     return(
-        <View>
-            <View>
+        <ScrollView style={{width:'100%'}}>
+            <View style={{width:'100%'}}>
+                
+                <ImageBackground source={require('../public/rooom.jpg')}>
+                    <View style={{width:'100%',height:300,justifyContent:'center',alignItems:'center'}}>
+                        <View style={{borderWidth:1,borderColor:'white',height:120,width:202,justifyContent:'center',alignItems:'center'}}>
+                            <Text style={{textAlign:'center',fontSize:24,color:'white'}}>OUR HOUSES</Text>
+                        </View>
+                    </View>
+                </ImageBackground>
+                <View style={{justifyContent:'center',alignItems:'center',marginTop:50,gap:40}}>
+                    {data.map(home => <HouseBox houseText={home.houseText} houseNumber={home.houseNumber} img={home.img} price={home.price} key={home.id}/>)}
+                </View>
 
             </View>
-        </View>
+        </ScrollView>
     )
 }
