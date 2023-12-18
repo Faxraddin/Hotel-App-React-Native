23d53d42845d569db41bf2cea4df3e23c6f01284
Faxraddin <reydivonay@gmail.com>
2023-12-19 00:15:01 +0400

    date choosing

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -1,23 +1,35 @@
-import { View,Text } from "react-native";
-import { Calendar as Cal,LocaleConfig} from 'react-native-calendars';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { View,Text } from 'react-native';
+import CalendarPicker from 'react-native-calendar-picker';
 
-export default function Calendar(){
+export default function Calendar() {
+    const [selectedStartDate, setSelectedStartDate] = useState([]);
+    const [selectedEndDate, setSelectedEndDate] = useState([]);
 
-    const [selected, setSelected] = useState('');
+    const handleChange = (date,type) => {
+        if(type === 'END_DATE') {
+            setSelectedEndDate(date);
+        } 
+        if(type === 'START_DATE') {
+            setSelectedStartDate(date);
+        }
+    }
 
-    return(
-        <View style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%',height:'100%'}}>
-            <View style={{width:'80%',height:'auto',padding:40,backgroundColor:'white',display:'flex',justifyContent:'center',alignItems:'center'}}>
-                <Cal
-                    onDayPress={day => {
-                        setSelected(day.dateString);
-                    }}
-                    markedDates={{
-                        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-                    }}
+    const mindate = new Date()
+
+    return (
+        <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+            <View style={{ width: '80%', height: 'auto', padding: 40, backgroundColor: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <CalendarPicker
+                    minDate={mindate}
+                    width={330}
+                    height={340}
+                    allowRangeSelection={true}
+                    onDateChange={handleChange}
                 />
+                <Text>SELECTED START DATE:{ selectedStartDate ? selectedStartDate.toString():'' }</Text>
+                <Text>SELECTED END DATE:{ selectedEndDate ? selectedEndDate.toString():'' }</Text>
             </View>
         </View>
-    )
+    );
 }
